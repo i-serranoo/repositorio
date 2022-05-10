47c209f64229eb3ac1d6504b3c51e4de0ec215d8
i-serranoo <iserranoortega@itba.edu.ar>
2022-05-10 08:53:43 -0300

    hjjkk

--- codigo.js
+++ codigo.js
@@ -1,2 +1,33 @@
- console.log("Hola mundo")
- 
+//Resolver problemas lógicos o matemáticos
+//La programación es una herramienta para resolver problemas
+//pensamiento computacional - PASOS:
+// primer etapa - descomponer el problema
+// segunda etapa - similitudes
+// tercer etapa - abstracción: achicar el problema, eliminar lo superfluo 
+// cuarta etapa - algoritmos: pasos para resolver el problema
+
+document.write("Tortas ejemplo")
+console.log("esto se debería ver únicamente en la consola y no en la página")
+
+/*
+Variables? Espacio en memoria donde se almacena un valor
+Es como un cajón
+¿Cómo armo una variable?
+let nombre DeLa Variable = "valor"
+let nombre = "carolina"
+las variables se pueden modificar
+let edad = 19
+edad = edad + 1
+ahora la variable edad tiene el valor de 20
+hay variables que no se pueden modificar porque son constantes 
+y se escriben así:
+const NOMBREDELAVARIABLE
+Tipos de datos:
+numérico: enteros (integral) o con coma (float)
+string: cadena de caracteres (con comillas)
+boolean: verdadero o falso
+el signo de mas (+) sirve tanto para la suma como para la concatenación (juntar strings)
+Java Script tiene por convencion Camel Case (primerLetraMinusculaYElRestoEnMayuscula)
+salvo las variables constantes que se escriben en MAYÚSCULA
+Python, por ejemplo, usa Snake Case (palabras_separadas_con_guion_bajo)
+/*
