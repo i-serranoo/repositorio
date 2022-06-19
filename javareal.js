a4e016364c2a5790f88d9c7fa87a7632504035b7
i-serranoo <iserranoortega@itba.edu.ar>
2022-06-19 18:11:15 -0300

    jcjh

--- javareal.js
+++ javareal.js
@@ -5,7 +5,6 @@
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
       this.classList.toggle("active");
-  
       let panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
